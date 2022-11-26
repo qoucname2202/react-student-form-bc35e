236be8004a8605fc9a4dd69c747612f64779c385
Nam Duong <duongquocnam224400@gmail.com>
2022-11-26 12:09:02 +0700

    Update done valid form

--- src/components/FormExercise/StudentForm/index.jsx
+++ src/components/FormExercise/StudentForm/index.jsx
@@ -1,11 +1,116 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
+import Swal from 'sweetalert2';
 class StudentForm extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			values: {
+				id: '',
+				fullName: '',
+				phone: '',
+				email: '',
+			},
+			errors: {
+				id: '',
+				fullName: '',
+				phone: '',
+				email: '',
+			},
+			valid: false,
+		};
+	}
+	handelOnInputChange = e => {
+		let newValue = this.state.values;
+		let { id, value } = e.target;
+		newValue[id] = value;
+		// Get data-type input
+		let type = e.target.getAttribute('data-type');
+		// Errors
+		let newErrors = this.state.errors;
+		let messageError = '';
+		if (value.trim() === '') {
+			messageError = `Please enter input !`;
+		} else {
+			if (type === 'number') {
+				let regex = /^[0-9]+$/;
+				if (!regex.test(value) || value.length < 10 || value.length >= 11) {
+					messageError = `Please enter a valid phone number.`;
+				}
+			} else if (type === 'email') {
+				let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+				if (!regex.test(value)) {
+					messageError = `Please enter a valid email address!`;
+				}
+			} else if (type === 'fullName') {
+				let regex =
+					/^[a-zA-Z_ÀÁÂÃÈÉÊẾÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶ" + "ẸẺẼỀỀỂưăạảấầẩẫậắằẳẵặẹẻẽềềểếỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợ" + "ụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ\\s]+$/;
+				if (!regex.test(value)) {
+					messageError = `Please enter a valid FullName!`;
+				}
+			} else if (type === 'id') {
+				let regex = /^[a-zA-Z0-9_ ]*$/;
+				if (!regex.test(value)) {
+					messageError = `Please enter a valid student code!`;
+				}
+			}
+		}
+		newErrors[id] = messageError;
+		this.setState(
+			{
+				values: newValue,
+				errors: newErrors,
+			},
+			() => {
+				this.setState({
+					valid: this.checkValid(),
+				});
+			},
+		);
+	};
+	checkValid = () => {
+		let { values, errors } = this.state;
+		for (const key in errors) {
+			if (errors[key] !== '' || values[key] === '') {
+				return false;
+			}
+		}
+		return true;
+	};
+	handelSubmit = e => {
+		e.preventDefault();
+		const id = document.getElementById('id').value;
+		const name = document.getElementById('fullName').value;
+		const phone = document.getElementById('phone').value;
+		const email = document.getElementById('email').value;
+		if (
+			id.length === 0 ||
+			name.length === 0 ||
+			phone.length === 0 ||
+			email.length === 0
+		) {
+			Swal.fire({
+				position: 'center',
+				icon: 'error',
+				title: 'Please Fill All Required Field',
+				showConfirmButton: false,
+				timer: 1500,
+			});
+		}
+		if (!this.checkValid()) {
+			return;
+		}
+		const action = {
+			type: 'ADD_USER',
+			payload: this.state,
+		};
+		this.props.dispatch(action);
+	};
 	render() {
+		// console.log(this.props.user);
 		return (
 			<>
-				<form>
+				<form onSubmit={this.handelSubmit}>
 					<div className='card'>
 						<div className='card-header bg-dark'>
 							<h4 className='text-white'>Thông tin sinh viên</h4>
@@ -15,48 +120,64 @@ class StudentForm extends Component {
 								<div className='col-6'>
 									<div className='form-group'>
 										<label htmlFor='id' className='form-label'>
-											Mã SV
+											Student code
 										</label>
 										<input
 											type='text'
+											data-type='id'
 											id='id'
 											className='form-control'
-											placeholder='Eg. 1234'
+											placeholder='Eg. 18DTHB2'
 											autoComplete='off'
+											onInput={this.handelOnInputChange}
 										></input>
-										{/* <div className='alert alert-danger mt-2'></div> */}
+										{this.state.errors.id !== '' && (
+											<div className='alert alert-danger mt-2'>
+												{this.state.errors.id}
+											</div>
+										)}
 									</div>
 								</div>
 								<div className='col-6'>
 									<div className='form-group'>
 										<label htmlFor='fullName' className='form-label'>
-											Họ tên
+											Fullname
 										</label>
 										<input
 											type='text'
+											data-type='fullName'
 											id='fullName'
 											className='form-control'
 											placeholder='Eg. Nguyễn Văn A'
 											autoComplete='off'
+											onInput={this.handelOnInputChange}
 										></input>
-										{/* <div className='alert alert-danger mt-2'></div> */}
+										{this.state.errors.fullName !== '' && (
+											<div className='alert alert-danger mt-2'>
+												{this.state.errors.fullName}
+											</div>
+										)}
 									</div>
 								</div>
 								<div className='col-6'>
 									<div className='form-group mt-3'>
 										<label htmlFor='phone' className='form-label'>
-											Số điện thoại
+											Phone number
 										</label>
 										<input
-											data-regex='^[0-9]+$'
 											data-type='number'
 											type='text'
 											id='phone'
 											className='form-control'
 											placeholder='Eg. 0917565841'
 											autoComplete='off'
+											onInput={this.handelOnInputChange}
 										></input>
-										{/* <div className='alert alert-danger mt-2'></div> */}
+										{this.state.errors.phone !== '' && (
+											<div className='alert alert-danger mt-2'>
+												{this.state.errors.phone}
+											</div>
+										)}
 									</div>
 								</div>
 								<div className='col-6'>
@@ -66,12 +187,18 @@ class StudentForm extends Component {
 										</label>
 										<input
 											type='text'
+											data-type='email'
 											id='email'
 											className='form-control'
 											placeholder='Eg. nguyenvana@gmail.com'
 											autoComplete='off'
+											onInput={this.handelOnInputChange}
 										></input>
-										{/* <div className='alert alert-danger mt-2'></div> */}
+										{this.state.errors.email !== '' && (
+											<div className='alert alert-danger mt-2'>
+												{this.state.errors.email}
+											</div>
+										)}
 									</div>
 								</div>
 							</div>
@@ -81,8 +208,10 @@ class StudentForm extends Component {
 										type='submit'
 										className='btn btn-success'
 										id='btnAddStudent'
+										disabled={!this.state.valid}
 									>
-										Thêm sinh viên
+										<i className='fa-solid fa-plus me-2'></i>
+										Add Student
 									</button>
 								</div>
 							</div>
--- src/redux/reducers/userReducer.jsx
+++ src/redux/reducers/userReducer.jsx
@@ -1,24 +1,27 @@
-const initialState = [
-	{
-		values: {
-			id: '',
-			fullName: '',
-			phone: '',
-			email: '',
+const initialState = {
+	userList: [
+		{
+			values: {
+				id: '',
+				fullName: '',
+				phone: '',
+				email: '',
+			},
+			errors: {
+				id: '',
+				fullName: '',
+				phone: '',
+				email: '',
+			},
+			valid: false,
 		},
-		errors: {
-			id: '',
-			fullName: '',
-			phone: '',
-			email: '',
-		},
-		valid: false,
-	},
-];
+	],
+};
 
 export const userReducer = (state = initialState, action) => {
 	switch (action.type) {
 		case 'ADD_USER': {
+			console.log(action.payload);
 			return state;
 		}
 		case 'DELETE_USER': {
